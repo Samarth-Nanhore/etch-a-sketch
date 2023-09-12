3e53159ec855e64526311143be9057f8eed2b4b6
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-09-12 20:29:57 +0530

    Create new element div and stored it in variable name gridElement in script.js
    - Add class name grid to gridElement
    - Add that new div element in DOM inside the gridContainer

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const gridContainer = document.createElement("div");
 gridContainer.style.cssText = "display: flex;";
 
 containerElement.appendChild(gridContainer);
+
+const gridElement = document.createElement("div");
+gridElement.classList.add("grid");
+
+gridContainer.appendChild(gridElement);
