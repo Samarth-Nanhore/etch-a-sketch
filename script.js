9b2c33eb0968a8246a956cd5819b171d2b3dc3f4
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-09-12 20:16:27 +0530

    Create new element div and stored it in variable name gridContainer in script.js
    - Add styling to gridContainer div element
    - Add that new div element in DOM inside the div element with class name .container

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const containerElement = document.querySelector(".container");
+
+const gridContainer = document.createElement("div");
+gridContainer.style.cssText = "display: flex;";
+
+containerElement.appendChild(gridContainer);
