eddb17a33388b885aa92ac39537bf984a3dd163e
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-09-13 23:45:33 +0530

    Add event listener to every gridElement from gridContainer
    - Add mouseover event to grid element that changes background color
    - Create variable name everyGridContainer that store every gridContainer from containerElement
    - Add class name grid container to gridContainer

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ let gridContainer;
 for (let i = 1; i <= 16; i++) {
   gridContainer = document.createElement("div");
   gridContainer.style.cssText = "display: flex;";
+  gridContainer.classList.add("gridcontainer");
   containerElement.appendChild(gridContainer);
 }
 
 let gridElement;
 
-containerElement.querySelectorAll("div").forEach((element) => {
+let everyGridContainer = containerElement.querySelectorAll("div");
+
+everyGridContainer.forEach((element) => {
   for (let i = 1; i <= 16; i++) {
     gridElement = document.createElement("div");
     gridElement.classList.add("grid");
     element.appendChild(gridElement);
   }
+});
+
+let gridElementsSelector = document.querySelectorAll("div.grid");
+
+gridElementsSelector.forEach((element) => {
+  element.addEventListener("mouseover", () => {
+    element.style.cssText = "background: #FFC436;";
+  });
 });
